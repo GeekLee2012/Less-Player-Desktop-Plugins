deb7c41b030fca76d11f27f8ae37f4716084f4e7
RIVE2012 <Rive08@126.com>
2024-10-20 20:13:06 +0800

    更新音源-Demo（空实现版）插件； 注：当前播放器的音源插件，不等同于音乐平台；音源插件更纯粹，仅提供歌曲音源切换，不提供专辑、歌单、歌手等功能，且可综合使用多个音乐平台；

--- 音源-Demo（空实现版）-WhoamI-v1.0.0.js
+++ 音源-Demo（空实现版）-WhoamI-v1.0.0.js
@@ -7,42 +7,74 @@
  */
 
 /* 默认提供的插件API */
-const { common, utils, crypto, events, nets, } = lessAPI
+const { common, utils, crypto, events, nets, permissions, } = lessAPI
 const { toMmss, toMillis } = utils
 const { randomTextDefault, aesEncryptText, rsaEncryptDefault } = crypto
 const { APIEvents, register, unregister } = events
 const { getDoc, getJson, postJson } = nets
+const { APIPermissions, access } = permissions
 
 
 
 
 /* 自由开发代码区 */
-const getExTrackPlayUrl = async track => {
-  //Demo音源，代码空实现
-  /*
-  return  {
-    url: null,  //播放URL
-    exurl: null //额外URL
-  }
-  */
+class Demo {
+    static CODE = "demo"
+
+    //获取歌曲播放详情：url、cover、lyric等
+    //注意：无法成功获取有效数据时，约定返回值为null
+    //返回值（属性列表）：{ ...track, url, cover, lyric, lyricTrans, lyricRoma }
+    static transferTrack(track, options) {
+      return new Promise((resolve, reject) => {
+        resolve(null)
+      })
+    }
+
 }
-
-
 
 /* 插件接入规范区 */
 //插件启用
 export const activate = () => {
-  /** 注册事件
-   * @param apiEvent 事件名称
-   * @param handler 事件处理函数，返回类型Promise
-   */
-  register(APIEvents.TRACK_GET_PLAY_URL, getExTrackPlayUrl)
+  //获取权限
+  access(APIPermissions.ADD_PLATFORM, { 
+    code: Demo.CODE,
+    vendor: Demo,
+    name: '自定义音源（空实现）',
+    shortName: 'DM',
+    online: true,
+    types: [],
+    scopes: ['united'], //使用范围：统一平台，即歌曲无法播放时，
+    artistTabs: [],
+    searchTabs: [],
+    weight: 8
+  })
+
+  //获取UserAgent
+  //const userAgent = await access(APIPermissions.GET_USER_AGENT)
+
+  access(APIPermissions.ADD_REQUEST_HANDLER, {
+    id: Demo.CODE,
+    hosts: ['abc.xyz'],
+    defaultHeaders: {
+        Origin: 'https://abc.xyz/',
+        Referer: 'https://abc.xyz/',
+    },
+    includes: [{
+      pattern: 'abc.xyz',
+      headers: {
+        Origin: 'abc.xyz',
+        Referer: 'abc.xyz/'
+      }
+    }]
+  })
+
   console.log('[ PLUGIN - Activated ] 音源 - Demo（空实现版）')
 }
 
 //插件停用
 export const deactivate = () => {
-  //注销事件
-  unregister(APIEvents.TRACK_GET_PLAY_URL, getExTrackPlayUrl)
+  //归还权限
+  access(APIPermissions.REMOVE_PLATFORM, Demo.CODE)
+  access(APIPermissions.REMOVE_REQUEST_HANDLER, Demo.CODE)
   console.log('[ PLUGIN - Deactivated ] 音源 - Demo（空实现版）')
 }
